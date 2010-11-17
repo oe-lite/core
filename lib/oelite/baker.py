feb13b4afdc7631a785528f03ac633d871396b44
Esben Haabendal <eha@doredevelopment.dk>
2010-11-17 11:21:21 +0100

    fix parser and fetch integration in bakery and add run_task support

--- lib/oelite/baker.py
+++ lib/oelite/baker.py
@@ -1,12 +1,13 @@
 import oebakery
 from oebakery import die, err, warn, info, debug
 from oelite import *
-import sys, os
-import glob, re
+import sys, os, glob, shutil
 
 from db import OEliteDB
 from recipe import OEliteRecipe
 from runq import OEliteRunQueue
+
+import bb.parse, bb.utils, bb.build, bb.fetch
 
 BB_ENV_WHITELIST = [
     "COLORTERM",
@@ -57,7 +58,7 @@ class OEliteBaker:
 
         self.import_env()
 
-        self.config = _parse("conf/bitbake.conf", config)
+        self.config = _parse("conf/bitbake.conf", self.config)
 
         # Handle any INHERITs and inherit the base class
         inherits  = ["base"] + (config.getVar("INHERIT", 1) or "").split()
@@ -65,7 +66,7 @@ class OEliteBaker:
             self.config = _parse("classes/%s.bbclass"%(inherit),
                                  self.config, 1)
 
-        #bb.fetch.fetcher_init(self.config)
+        bb.fetch.fetcher_init(self.config)
 
         self.appendlist = {}
 
@@ -89,7 +90,7 @@ class OEliteBaker:
         for var in whitelist:
             if not var in self.config and var in os.environ:
                 self.config[var] = os.environ[var]
-                #debug("importing env %s=%s"%(var, os.environ[var]))
+                debug("importing env %s=%s"%(var, os.environ[var]))
         return
 
 
@@ -182,18 +183,24 @@ class OEliteBaker:
             #    debug("runq_taskdepend %s\t%s\t%s\t%s\t%s\t%s"%runable)
             recipe_name = self.db.get_recipe({"task": task})
             task_name = self.db.get_task(task=task)
-            info("Running %s:%s"%(recipe_name,task_name))
-            runq.mark_done(task)
+            debug("")
+            info("Running %s:%s"%(recipe_name, task_name))
+            if self.run_task(task):
+                runq.mark_done(task)
+            else:
+                warn("%s:%s failed"%(recipe_name, task_name))
+                # FIXME: support command-line option to abort on first
+                # failed task
             task = runq.get_runabletask()
 
         return 0
 
 
     def run_task(self, task):
-        # FIXME: why?
-        os.umask(022)
-
-        return
+        recipe_id = self.db.get_recipe_id(task=task)
+        recipe = self.cookbook[recipe_id]
+        task_name = self.db.get_task(task=task)
+        return exec_func(task_name, recipe.data)
 
 
     def setup_tmpdir(self):
@@ -264,4 +271,199 @@ class OEliteBaker:
 
     def parse_recipe(self, recipe):
         path = os.path.abspath(recipe)
-        return bb.parse.handle(recipe, self.config.createCopy())
+        return bb.parse.handle(path, self.config.createCopy())
+
+
+def exec_func(func, data):
+
+    body = data.getVar(func, True)
+    if not body:
+        return True
+
+    flags = data.getVarFlags(func)
+    for item in ['deps', 'check', 'interactive', 'python', 'fakeroot',
+                 'cleandirs', 'dirs']:
+        if not item in flags:
+            flags[item] = None
+
+    ispython = flags['python']
+
+    cleandirs = flags['cleandirs']
+    if cleandirs:
+        cleandirs = data.expand(cleandirs, None).split()
+    if cleandirs:
+        for cleandir in cleandirs:
+            if not os.path.exists(cleandir):
+                continue
+            try:
+                debug("cleandir %s"%(cleandir))
+                shutil.rmtree(cleandir)
+            except Exception, e:
+                err("cleandir %s failed: %s"%(cleandir, e))
+                return False
+
+    dirs = flags['dirs']
+    if dirs:
+        dirs = data.expand(dirs, None).split()
+
+    if dirs:
+        for adir in dirs:
+            bb.utils.mkdirhier(adir)
+        adir = dirs[-1]
+    else:
+        adir = data.getVar('B', True)
+
+    # Save current directory
+    try:
+        prevdir = os.getcwd()
+    except OSError:
+        prevdir = data.getVar('TOPDIR', True)
+
+    # Setup logfiles
+    t = data.getVar('T', 1)
+    if not t:
+        die("T variable not set, unable to build")
+    bb.utils.mkdirhier(t)
+    logfile = "%s/log.%s.%s" % (t, func, str(os.getpid()))
+    runfile = "%s/run.%s.%s" % (t, func, str(os.getpid()))
+
+    # Change to correct directory (if specified)
+    if adir and os.access(adir, os.F_OK):
+        os.chdir(adir)
+
+    # stdin
+    si = file('/dev/null', 'r')
+
+    # stdout
+    try:
+        if oebakery.DEBUG or ispython:
+            so = os.popen("tee \"%s\"" % logfile, "w")
+        else:
+            so = file(logfile, 'w')
+    except OSError:
+        logger.exception("Opening log file '%s'", logfile)
+        pass
+
+    # stderr
+    se = so
+
+    # Dup the existing fds so we dont lose them
+    osi = [os.dup(sys.stdin.fileno()), sys.stdin.fileno()]
+    oso = [os.dup(sys.stdout.fileno()), sys.stdout.fileno()]
+    ose = [os.dup(sys.stderr.fileno()), sys.stderr.fileno()]
+
+    # Replace those fds with our own
+    os.dup2(si.fileno(), osi[1])
+    os.dup2(so.fileno(), oso[1])
+    os.dup2(se.fileno(), ose[1])
+
+    # FIXME: why?
+    os.umask(022)
+
+    try:
+        # Run the function
+        retval = False
+        if ispython:
+            retval = exec_func_python(func, data, runfile, logfile)
+        else:
+            retval = exec_func_shell(func, data, runfile, logfile, flags)
+
+    finally:
+
+        # Restore original directory
+        try:
+            os.chdir(prevdir)
+        except:
+            pass
+
+        # Restore the backup fds
+        os.dup2(osi[0], osi[1])
+        os.dup2(oso[0], oso[1])
+        os.dup2(ose[0], ose[1])
+
+        # Close our logs
+        si.close()
+        so.close()
+        se.close()
+
+        if os.path.exists(logfile) and os.path.getsize(logfile) == 0:
+            debug("Removing zero size logfile: %s"%logfile)
+            os.remove(logfile)
+
+        # Close the backup fds
+        os.close(osi[0])
+        os.close(oso[0])
+        os.close(ose[0])
+
+    return retval
+
+
+def exec_func_python(func, data, runfile, logfile):
+    """Execute a python BB 'function'"""
+
+    bbfile = data.getVar("FILE", True)
+    tmp  = "def " + func + "(d):\n%s" % data.getVar(func, True)
+    tmp += '\n' + func + '(d)'
+
+    f = open(runfile, "w")
+    f.write(tmp)
+    comp = bb.utils.better_compile(tmp, func, bbfile)
+    try:
+        bb.utils.better_exec(comp, {"d": data}, tmp, bbfile)
+    except:
+        if oebakery.DEBUG:
+            raise
+        return False
+        #if sys.exc_info()[0] in (bb.parse.SkipPackage, bb.build.FuncFailed):
+        #    raise
+        ##return False
+        #raise
+
+    return True
+
+
+def exec_func_shell(func, data, runfile, logfile, flags):
+    """Execute a shell BB 'function' Returns true if execution was successful.
+
+    For this, it creates a bash shell script in the tmp dectory,
+    writes the local data into it and finally executes. The output of
+    the shell will end in a log file and stdout.
+
+    Note on directory behavior.  The 'dirs' varflag should contain a list
+    of the directories you need created prior to execution.  The last
+    item in the list is where we will chdir/cd to.
+    """
+
+    deps = flags['deps']
+    check = flags['check']
+    if check in globals():
+        if globals()[check](func, deps):
+            return
+
+    f = open(runfile, "w")
+    f.write("#!/bin/sh -e\n")
+    #if oebakery.DEBUG:
+    #    f.write("set -x\n")
+    bb.data.emit_env(f, data)
+
+    f.write("cd %s\n" % os.getcwd())
+    if func: f.write("%s\n" % func)
+    f.close()
+    os.chmod(runfile, 0775)
+    if not func:
+        raise TypeError("Function argument must be a string")
+
+    # execute function
+    if flags['fakeroot']:
+        maybe_fakeroot = "PATH=\"%s\" %s " % (data.getVar("PATH", True),
+                                              data.getVar("FAKEROOT", True)
+                                              or "fakeroot")
+    else:
+        maybe_fakeroot = ''
+    lang_environment = "LC_ALL=C "
+    ret = os.system('%s%ssh -e %s'%(lang_environment, maybe_fakeroot, runfile))
+
+    if ret == 0:
+        return True
+
+    return False
