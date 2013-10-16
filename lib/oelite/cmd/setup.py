9c46ccff0cd197a0b9a42321ee6b0d308f197123
Esben Haabendal <esben.haabendal@prevas.dk>
2013-10-16 08:14:01 +0200

    lib/oelite: Reshape setup command
    * Rename -p/--printcmd to -n/--dryrun
    * Add -y/--yes option, for running command without prompting
    * Rename "host configurations" to "setup files"
    * Look for files in all layers
    * Ignore setup files that look like backup files and such when scanning
    * Require first line in setup file to be a comment line with host distribution
      name / description
    * Support empty (as in, only first description line) to mark known unsupported
      host distributions
    * Add version fallback when choosing setup file, so if host distribution is
      detected as foobar 1.2.3, first try foobar_1.2.3, then fallback to
      foobar_1.2, then foobar_1 and finally foobar
    * Fix "set -ex" usage when running the setup file
    
    Signed-off-by: Esben Haabendal <esben.haabendal@prevas.dk>
    
    (cherry picked from commit d1e2f5d90b4359c044009a5db006754b77b09374)

--- lib/oelite/cmd/setup.py
+++ lib/oelite/cmd/setup.py
@@ -1,6 +1,5 @@
 import oebakery
 from oelite import util
-from oebakery import info
 import logging
 import os
 import subprocess
@@ -8,138 +7,231 @@ import oelite
 import bb
 import glob
 import sys
+import re
+import tempfile
 
-description = "Host preparation tool"
+description = "Setup host for use with this OE-lite manifest"
 arguments = ()
 
 def add_parser_options(parser):
-    parser.add_option("-l", "--list",
-                      action="store_true", default=False,
-                      help="Show a list of known host configurations")
-    parser.add_option("-p", "--printcmd",
-                    action="store_true", default=False,
-                      help="Print the command needed to prepare the host for OE-lite")
-    parser.add_option("-d", "--debug",
-                      action="store_true", default=False,
-                      help="Verbose output")
+    parser.add_option(
+        '-l', '--list', action='store_true', default=False,
+        help="Show a list of known host distributions")
+    parser.add_option(
+        '-n', '--dryrun', action='store_true', default=False,
+        help="Only show the commands needed to setup the distrbution")
+    parser.add_option(
+        '-y', '--yes', action='store_true', default=False,
+        help="Run command, without waiting for 'y' response")
+    parser.add_option(
+        '-d', '--debug', action='store_true', default=False,
+        help="Verbose output")
     return
-
 
 def parse_args(options, args):
     if options.debug:
         logging.getLogger().setLevel(logging.DEBUG)
     else:
         logging.getLogger().setLevel(logging.INFO)
-    if args:
-        options.head = args.pop(0)
-    else:
-        options.head = None
     return
 
-def get_host_configs(distro, release, config):
-    if(release  == "unknown"):
-        release = "none"
-    logging.debug("Reading host configuration for: %s-%s", distro, release)
-    oepath = config.get("OEPATH")
-    logging.debug("Searching for setup in paths; %s", oepath)
-    if(distro == "all"):
-        path = bb.utils.which(oepath, "setup/")
-        files = glob.glob(path+"*")
+def parse_setup_file(filename):
+    name = os.path.basename(filename)
+    if not re.match('[a-zA-Z].*[^#~]$', name):
+        return
+    if '_' in name:
+        distro, release = name.split('_', 1)
     else:
-        files = bb.utils.which(oepath, "setup/"+distro+"_"+release)
-    logging.debug("Found file(s): "+str(files))
-    return files
+        distro = name
+        release = ''
+    if os.path.getsize(filename) == 0:
+        description = ''
+        supported = False
+    else:
+        with open(filename, 'r') as f:
+            description = f.readline()
+            if not description.startswith('#'):
+                logging.warning("Invalid setup file: %s", filename)
+                return
+            description = description.lstrip('#').strip()
+            script = f.read()
+            if not script.strip():
+                supported = False
+            else:
+                supported = True
+    return (name, description, supported, filename)
 
-def determine_host(oeconfig):
-    logging.debug("Running host detection")
+def get_setup_files(config):
+    logging.debug("Looking for distribution setup files")
+    oepath = config.get("OEPATH")
+    logging.debug("Looking in: %s", oepath)
+    supported_distros = {}
+    unsupported_distros = {}
+    found_distros = set()
+    for p in oepath.split(':'):
+        setup_dir = os.path.join(p, 'setup')
+        if os.path.isdir(setup_dir):
+            for f in glob.glob(os.path.join(setup_dir, '*')):
+                setup_file = parse_setup_file(f)
+                if not setup_file:
+                    continue
+                name, description, supported, filename = setup_file
+                if name in found_distros:
+                    continue
+                found_distros.add(name)
+                if supported:
+                    supported_distros[name] = (description, filename)
+                else:
+                    unsupported_distros[name] = (description, filename)
+    return supported_distros, unsupported_distros
+
+def get_setup_file(config, distro, release):
+    if release is None:
+        name = distro
+    else:
+        name = '%s %s'%(distro, release)
+    def retval(f):
+        setup_file = parse_setup_file(f)
+        if not setup_file:
+            return None, None
+        name, description, supported, filename = setup_file
+        return filename, supported
+    logging.debug("Looking for setup file for %s", name)
+    oepath = config.get("OEPATH")
+    logging.debug("Looking in: %s", oepath)
+    # Look for release specific setup file first, falling back to common
+    # versions.  When fx. searching for debian 7.1, first look for debian_7.1,
+    # then debian_7.
+    if release is not None:
+        release_parts = release.split('.')
+        for i in reversed(range(len(release_parts))):
+            setup_file = bb.utils.which(oepath, os.path.join(
+                    'setup', '%s_%s'%(distro, '.'.join(release_parts[:i+1]))))
+            if setup_file:
+                return retval(setup_file)
+    # For rolling release distros, and in case a version specific setup file
+    # was not found, look for version neutral setup file.
+    setup_file = bb.utils.which(oepath, os.path.join('setup', distro))
+    return retval(setup_file)
+
+def get_host_distro():
+    logging.debug("Running host distribution detection")
     logging.debug("Searching for lsb_release information")
-    #try lsb-release which is the most widely available method
+    # try lsb-release which is the most widely available method
+    logging.debug("Checking for the lsb_release binary")
     try:
-        logging.debug("checking for the lsb_release binary")
-        subprocess.call(["lsb_release"])
+        subprocess.call(["lsb_release"], stderr=open(os.devnull, 'w'))
     except OSError as e:
         if e.errno == os.errno.ENOENT:
             # handle file not found error.
             logging.debug("lsb_release binary not available")
         else:
-            logging.debug("unhandled exception when calling lsb_release")
+            logging.debug("Unhandled exception when calling lsb_release")
             raise
     else:
+        logging.debug("Using lsb_release to get host information")
         try:
-            logging.debug("using lsb_release to get host information")
             #strip newlines and make lowercase for later matching
-            distro = oelite.util.shcmd("lsb_release -si", quiet=True)[:-1].lower()
-            release = oelite.util.shcmd("lsb_release -sr", quiet=True)[:-1].lower()
+            distro = oelite.util.shcmd(
+                "lsb_release -si", quiet=True)[:-1].lower()
+            release = oelite.util.shcmd(
+                "lsb_release -sr", quiet=True)[:-1].lower()
         except:
-            logging.debug("unhandled exception when calling lsb_release")
+            logging.debug("Unhandled exception when calling lsb_release")
             raise
         else:
-            logging.debug("lsb_release: distro: %s | release: %s", distro, release)
+            logging.debug("lsb_release: distro=%s release=%s", distro, release)
             return distro, release
 
-    logging.debug("No lsb_release information available - checking for other known host configurations")
+    logging.debug("No lsb_release information available")
+    logging.debug("Checking for other known host distributions")
     logging.debug("Checking for Exherbo")
-    if(os.path.exists("/etc/exherbo-release") and os.path.isfile("/etc/exherbo-release")):
-        return "exherbo", "unknown"
-    logging.debug("No host information for this host")
-    return "unknown","unknown"
+    if (os.path.exists("/etc/exherbo-release") and
+        os.path.isfile("/etc/exherbo-release")):
+        return "exherbo", None
+
+    logging.debug("Unable to determine host distribution")
+    return None, None
 
 def run(options, args, config):
     logging.debug("setup.run %s", options)
 
     if(options.list):
-        print "List of known host configurations:\n"
-        hosts = get_host_configs("all", "", config)
-        logging.debug("got following config files: ")
-        logging.debug(hosts)
-        for host in hosts:
-            file = host.split("/")[-1]
-            distro = file.split("_")[0]
-            release = file.split("_")[1]
-            print '{0:8}{1:15}{2:9}{3:15}{4:18}{5}'.format("Distro:", distro, "Release:", release,  "Setup command in: ", host)
+        def list_distros(distros):
+            for name in sorted(distros.keys()):
+                description, filename = distros[name]
+                if options.debug:
+                    print "  %s (%s)"%(description, filename)
+                else:
+                    print "  %s"%(description)
+        supported, unsupported = get_setup_files(config)
+        if unsupported:
+            print "\nUnsupported hosts distributions:"
+            list_distros(unsupported)
+        if supported:
+            print "\nSupported hosts distributions:"
+            list_distros(supported)
         return 0
 
-    #try automatic host detection
-    distro, release = determine_host(config)
-    if(options.printcmd):
-        logging.debug("printing command for distro: %s release: %s", distro, release)
-        cmd_file = get_host_configs(distro, release, config)
-        cmd = open(cmd_file, 'r').read()[:-1]
-        logging.debug("Command: '%s'", cmd)
-        print cmd
-        return 0
-    if(distro == "unknown"):
-        info("Your host is not known by the oe setup tool.")
-        info("Try running 'oe setup -l' to see if a known configuration could match your host")
+    distro, release = get_host_distro()
+    if distro is None:
+        logging.error("Unable to detect host distribution")
+        return 1
+    print "Host distribution: %s %s"%(distro, release if release else '')
+
+    setup_file, supported = get_setup_file(config, distro, release)
+    if not setup_file:
+        logging.error("Unable to find setup file for your host distribution")
+        logging.error("Use 'oe setup -l' to see available setup files")
+        return 1
+    if not supported:
+        logging.error("Setup on your host distribution is not supported")
+        return 1
+
+    logging.debug("Setup file: %s", setup_file)
+    with open(setup_file, 'r') as f:
+        f.readline() # skip first line, it contains setup file description
+        script_body = f.read().strip()
+    print "\nThe following commands %s setup your host distribution"%(
+        'should' if options.dryrun else 'will')
+    print '-'*79
+    print script_body
+    print '-'*79 + '\n'
+    if options.dryrun:
+        print "Run them manually, or run setup without -n/--dryrun"
         return 0
 
-    info("Your host configuration detected to be:\n")
-    info("Distro: "+distro+"\nRelease: "+release+"\n")
-    #try exact match
-    cmd_file = get_host_configs(distro, release, config)
-    if(not cmd_file):
-        #try major version match
-        cmd_file = get_host_configs(distro, release[0]+"*", config)
-        if(not cmd_file):
-            info("Your distribution is known, but the release can not be matched to any known configuration")
-            info("Try running 'oe setup -l' to see if a known configuration could match your host")
-            return 0
-    cmd = open(cmd_file, 'r').read()[:-1]
-    info("The following command will setup your host for OE-lite:\n")
-    info(cmd)
-    if os.isatty(sys.stdin.fileno()):
+    if options.yes:
+        response = 'y'
+    elif os.isatty(sys.stdin.fileno()):
         while True:
             try:
-                response = raw_input("\nDo you want to run this command [Yes/yes/no]? ")
+                response = raw_input(
+                    "Do you want to run these commands [Y/n]? ")
             except KeyboardInterrupt:
-                response = "no"
-            if response in ("yes", "Yes"):
-                print ""
-                oelite.util.shcmd("set -ex", quiet=True)
-                oelite.util.shcmd(cmd, quiet=False)
+                print
+                response = 'n'
+            response = response.lower()
+            if response == '':
+                response = 'y'
+            if response in ('y', 'n'):
+                print
                 break
-            else:
-                info("Not running command - answer strictly 'yes' or 'Yes' to run the command automatically")
-                return 0
 
-    return 0
+    if response == 'n':
+        print "Maybe another time"
+        return 0
+    assert response == 'y'
+
+    script = tempfile.NamedTemporaryFile(prefix='setup.', delete=False)
+    script.write("#!/bin/sh\nset -ex\n" + script_body + '\n')
+    script.close()
+    script = script.name
+    os.chmod(script, 0755)
+    success = oelite.util.shcmd(script, quiet=False)
+    if success:
+        os.unlink(script)
+        print "\nHost is now ready to use with this OE-lite manifest"
+        return 0
+    else:
+        return 1
