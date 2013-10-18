oe-lite/core

commit c885658376e546826e18dd84daee47978b9cd863

lib/oelite: Set EXTRA_ARCH when variables use MACHINE override
When recipe metadata has one or more variables that has a value that is
altered by a MACHINE_${MACHINE} override, the EXTRA_ARCH variable is set to
'.${MACHINE}'.

This is similar to what happens on file ('file://' machine override), and
machine USE flag settings.

Signed-off-by: Esben Haabendal <esben.haabendal@prevas.dk>

lib/oelite/meta/dict.py
@@ -166,23 +166,33 @@ class DictMeta(MetaData):
             oval = None
             append = ""
             prepend = ""
+            var_override_used = None
+            overrides_used = set()
             for override in current_overrides:
                 if oval is None:
                     try:
                         oval = var_overrides[override]
+                        var_override_used = override
                     except KeyError:
                         pass
                 try:
                     append += append_overrides[override] or ""
+                    overrides_used.add(override)
                 except KeyError:
                     pass
                 try:
                     prepend = prepend_overrides[override] or "" + prepend
+                    overrides_used.add(override)
                 except KeyError:
                     pass
             if oval is not None:
                 val = oval
+                overrides_used.add(var_override_used)
             val = prepend + (val or "") + append
+            for override in overrides_used:
+                if override.startswith('MACHINE_'):
+                    self.dict['EXTRA_ARCH'] = '.%s'%(self.dict['MACHINE'])
+                    break
 
         if expand == OVERRIDES_EXPANSION:
             return (val, None)

commit 935ffeedda03912da544eb1bd3b10116d0ea0d9e

lib/oelite: Fix bug in MACHINE override handling
Fix bug introduced by c885658376e546826e18dd84daee47978b9cd863
causing a crash when parsing recipes where MACHINE_${MACHINE} override
causes changes to EXTRA_ARCH.

Signed-off-by: Esben Haabendal <esben.haabendal@prevas.dk>

lib/oelite/meta/dict.py
@@ -191,7 +191,7 @@ class DictMeta(MetaData):
             val = prepend + (val or "") + append
             for override in overrides_used:
                 if override.startswith('MACHINE_'):
-                    self.dict['EXTRA_ARCH'] = '.%s'%(self.dict['MACHINE'])
+                    self['EXTRA_ARCH'] = '.%s'%(self['MACHINE'])
                     break
 
         if expand == OVERRIDES_EXPANSION: