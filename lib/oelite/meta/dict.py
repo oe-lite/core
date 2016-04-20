67e78486daebe7ff398b085d3b6a68aeecba6774
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2016-04-20 10:49:12 +0200

    oelite: meta/dict.py: remove assert
    We're calling _get which contains the same assert. Since we're called
    nearly 2.5M times during oe bake world, this adds several seconds of
    useless overhead (should the assert in _get fire, get will be in the
    backtrace).

--- lib/oelite/meta/dict.py
+++ lib/oelite/meta/dict.py
@@ -166,7 +166,6 @@ class DictMeta(MetaData):
 
     def get(self, var, expand=FULL_EXPANSION):
         #print "get var=%s expand=%s"%(var, expand)
-        assert isinstance(expand, int)
         val = self._get(var, expand)[0]
         #print "get returning %s"%(val)
         return val
