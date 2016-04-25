5e56743844a084ab3220400117024ee3c380e78a
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2016-04-25 11:01:09 +0200

    lib/oelite/dbutil.py: remove now useless code
    Since 4aeacaaa11 ("oelite: cookbook.py: get str objects from sqlite,
    not unicode"), we're not getting unicode objects from the database, so
    these checks never trigger.

--- lib/oelite/dbutil.py
+++ lib/oelite/dbutil.py
@@ -4,8 +4,6 @@ def flatten_single_value(rows):
     row = rows.fetchone()
     if row is None:
         return None
-    if isinstance(row[0], unicode):
-        return str(row[0])
     return row[0]
 
 def flatten_single_column_rows(rows):
@@ -13,10 +11,7 @@ def flatten_single_column_rows(rows):
     if not rows:
         return []
     for i in range(len(rows)):
-        if isinstance(rows[i][0], unicode):
-            rows[i] = str(rows[i][0])
-        else:
-            rows[i] = rows[i][0]
+        rows[i] = rows[i][0]
     return rows
 
 def var_to_tuple(v):
