188ad20d0cf25beedac1d086b5bb7480b03b9092
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2016-11-18 10:54:22 +0100

    bb/data.py: remove unused functions
    Only bb.data.getVar is used. We'll get rid of that later, but for now
    just kill the completely dead code.

--- lib/bb/data.py
+++ lib/bb/data.py
@@ -4,33 +4,5 @@
 BitBake Data API wrapper for OE-lite
 """
 
-import sys
-import warnings
-
-def setVar(var, value, d):
-    d.set(var, value)
-
 def getVar(var, d, exp=0):
     return d.get(var, exp)
-
-def setVarFlag(var, flag, value, d):
-    d.set_flag(var, flag, value)
-
-def getVarFlag(var, flag, d):
-    return d.get_flag(var, flag)
-
-def expand(s, d):
-    return d.expand(s)
-
-def inherits_class():
-    raise Exception("bb.data.inherits_class() not implemented")
-
-def emit_env(o=sys.__stdout__, d=None):
-    return d.dump(o=o)
-
-def createCopy(d):
-    return d.copy()
-
-def update_data():
-    warnings.warn("update_data() is deprecated")
-    return
